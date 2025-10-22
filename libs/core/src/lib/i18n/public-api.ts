bed364ca7aac59d017f7d3ea4c41c122c91b8d11
Mustafa DURAN <mustafa.duran@suenohotels.local>
2025-10-22 19:24:40 +0300

    feat(i18n): export all public APIs via @core/i18n

---
+++ libs/core/src/lib/i18n/public-api.ts
@@ -0,0 +1,6 @@
+export * from './i18n.component';
+export * from './i18n.constants';
+export * from './i18n.interceptor';
+export * from './i18n.pipe';
+export * from './i18n.service';
+export * from './i18n.types';
