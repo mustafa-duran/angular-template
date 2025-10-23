043e481f072966f2cef7ff466a2cf0f25e175c44
Mustafa <mustafa@Mustafas-MacBook-Air.local>
2025-10-23 19:22:29 +0300

    feat(web): configure default route to homepage

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/homepage/homepage').then((m) => m.Homepage),
+    pathMatch: 'full',
+    title: 'Homepage'
+  }
+];
